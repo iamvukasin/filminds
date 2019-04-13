2225f1b0a794fddd0ae7b1e78e67c565e56d6fa4
Sanja Mijovic <sanjamijovic97@gmail.com>
2019-04-13 17:35:29 +0200

    Add register view

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import IndexView, RegisterView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('register/', RegisterView.as_view(), name='register')
 ]
