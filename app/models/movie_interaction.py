20b241825a25e3a33bfcdf8be6de25cac6f0d1b5
NemanjaNeca <34663029+NemanjaNeca@users.noreply.github.com>
2019-04-16 00:25:54 +0200

    add nullability for user in SearchedMovie model

--- app/models/movie_interaction.py
+++ app/models/movie_interaction.py
@@ -6,7 +6,7 @@ from .expert_picks import ExpertPicksCategory
 
 
 class SearchedMovie(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None, blank=True, null=True)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     count = models.IntegerField(default=0)
 
