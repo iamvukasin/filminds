f8d2896dbc274f8150b646aeedc5290eea44f339
NemanjaNeca <34663029+NemanjaNeca@users.noreply.github.com>
2019-04-15 13:10:22 +0200

    add model for watch and wish movies

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,4 +2,4 @@ from .expert_picks import ExpertPicksCategory
 from .message import Message
 from .movie import MovieGenre, Movie
 from .user import User
-from .movie_interaction import SearchedMovie
+from .movie_interaction import SearchedMovie, CollectedMovie
--- app/models/movie_interaction.py
+++ app/models/movie_interaction.py
@@ -8,3 +8,20 @@ class SearchedMovie(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     count = models.IntegerField(default=0)
+
+
+class CollectedMovie(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    date = models.DateField()
+    TYPE_WATCH = 'WA'
+    TYPE_WISH = 'WI'
+    TYPE_CHOICES = (
+        (TYPE_WATCH, 'Watch list'),
+        (TYPE_WISH, 'Wish list')
+    )
+    type = models.CharField(
+        choices=TYPE_CHOICES,
+        max_length=2,
+        default=TYPE_WISH
+    )
