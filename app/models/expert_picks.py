e56b1dc55b71220124a369f335f0d1bcb23b134d
Vukašin Manojlović <iamvukasin@gmail.com>
2019-06-03 15:07:32 +0200

    Show only expert picks from categories which have movies

--- app/models/expert_picks.py
+++ app/models/expert_picks.py
@@ -7,10 +7,24 @@ class ExpertPicksCategory(models.Model):
     name = models.CharField(max_length=30)
     expert = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
 
-    @staticmethod
-    def get_all():
-        return ExpertPicksCategory.objects.exclude(expert__isnull=True).values()
+    class Meta:
+        ordering = ['name']
 
     @staticmethod
-    def get_first():
-        return ExpertPicksCategory.objects.exclude(expert__isnull=True).first()
+    def _get_valid_categories():
+        """
+        Returns categories which have a dedicated expert and have at
+        least one movie added.
+
+        :return: the list of valid categories
+        """
+
+        return ExpertPicksCategory.objects.filter(expert__isnull=False, expertpickmovie__isnull=False).distinct()
+
+    @classmethod
+    def get_all(cls):
+        return cls._get_valid_categories().values()
+
+    @classmethod
+    def get_first(cls):
+        return cls._get_valid_categories().first()
